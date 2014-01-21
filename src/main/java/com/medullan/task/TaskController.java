2c764766939f527b228346d6795dd4aa681f7af5
Denis Bell <denisdbell@gmail.com>
2014-01-21 04:27:36 -0400

    commit chaes to the class

--- src/main/java/com/medullan/task/TaskController.java
+++ src/main/java/com/medullan/task/TaskController.java
@@ -20,59 +20,8 @@ public class TaskController {
 	public String setupForm(Map<String,Object> map){
 		map.put("task", task);
 		map.put("taskList", taskService.getAll());
+		map.put("taskListTwo", taskService.getAll());
 		return "task";
 	}
-	
-//	@RequestMapping(value="/student.do",method=RequestMethod.POST)
-//	public String doActions(@ModelAttribute Student student,BindingResult result,
-//		   @RequestParam String action, Map<String,Object> map){
-//		
-//		Student searchedStudent  = null;
-//		Student studentResult = null;
-//		
-//		
-//		
-////		switch(action.toLowerCase()){
-////			case "add":
-////				studentService.add(student);
-////			break;
-////			case "edit":
-////				studentService.edit(student);
-////			break;
-////			case "delete":
-////				studentService.delete(student.getStudentid());
-////			break;
-////			case "search":
-////				searchedStudent = studentService.get(student.getStudentid());
-////			break;
-////		}
-//		
-//		if(searchedStudent != null){
-//			studentResult = searchedStudent;
-//		}else{
-//			studentResult = student;
-//		}
-//		
-//		map.put("student", studentResult);
-//		map.put("studentList", studentService.getAll());
-//		return "student";
-//	}
-//	
-//	//Getters and Setter
-//	public StudentService getStudentService() {
-//		return studentService;
-//	}
-//
-//	public void setStudentService(StudentServiceImpl studentService) {
-//		this.studentService = studentService;
-//	}
-//
-//	public Student getStudent() {
-//		return student;
-//	}
-//
-//	public void setStudent(Student student) {
-//		this.student = student;
-//	}
 
 }
